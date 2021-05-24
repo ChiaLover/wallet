b1e0c1a3807059ff36081a2b74061ed039fb06cd
Phuong Lam <lamtungphuongtv@gmail.com>
2021-05-24 11:52:05 -0400

    Add flutter - bls

---
+++ ios/Runner/wrapper.cpp
@@ -0,0 +1,98 @@
+//
+//  wrapper.cpp
+//  Runner
+//
+//  Created by Phuong Lam on 5/9/21.
+//
+
+#include <stdint.h>
+#include <stdio.h>
+#include "bls.hpp"
+
+#define PRIVATE_KEY_LEN 32
+#define PUBLIC_KEY_LEN 48
+#define SIGNATURE_LEN 96
+
+using namespace std;
+
+void printKey(vector<uint8_t> key) {
+    for (int i = 0; i < key.size(); i++) {
+        cout << unsigned(key[i]) << ", ";
+    }
+    cout << endl;
+}
+
+vector<uint8_t> append(vector<uint8_t> a, vector<uint8_t> b) {
+    
+    a.insert(a.end(), b.begin(), b.end());
+    return a;
+}
+
+extern "C" __attribute__((visibility("default"))) __attribute__((used))
+uint8_t* key_gen() {
+    std::vector<uint8_t> seed = {0,  51, 6,  244, 24,  199, 1,  25,  52,  88,  192,
+                                19, 18, 12, 89,  6,   220, 18, 102, 58,  209, 82,
+                                12, 62, 89, 110, 182, 9,   44, 20,  254, 22};
+    
+    bls::PrivateKey sk = bls::AugSchemeMPL().KeyGen(seed);
+    bls::G1Element pk = sk.GetG1Element();
+    
+    cout << bls::Util::HexStr(sk.Serialize()) << endl;
+    cout << bls::Util::HexStr(pk.Serialize()) << endl;
+    
+    vector<uint8_t> data;
+    data.push_back(0);
+    
+    vector<uint8_t> skB = sk.Serialize();
+    vector<uint8_t> pkB = pk.Serialize();
+    data.insert(data.end(), skB.begin(), skB.end());
+    data.insert(data.end(), pkB.begin(), pkB.end());
+
+    uint8_t* key = static_cast<uint8_t*>(malloc(81));
+    copy(data.begin(), data.end(), key);
+    return key;
+}
+
+extern "C" __attribute__((visibility("default"))) __attribute__((used))
+uint8_t* sign(uint8_t* key, uint8_t* message, size_t messageLen) {
+    
+    cout << bls::Util::HexStr(key, 32) << endl;
+    cout << bls::Util::HexStr(key, 32) << endl;
+
+    bls::Bytes keyBytes = bls::Bytes(key, PRIVATE_KEY_LEN);
+    bls::PrivateKey sk = bls::PrivateKey::FromBytes(keyBytes);
+    
+    bls::G2Element signature = bls::AugSchemeMPL().Sign(sk, bls::Bytes(message, messageLen));
+    vector<uint8_t> s = signature.Serialize();
+    
+    uint8_t* sign = static_cast<uint8_t*>(malloc(SIGNATURE_LEN));
+    
+    copy(s.begin(), s.end(), sign);
+    
+    return sign;
+}
+
+extern "C" __attribute__((visibility("default"))) __attribute__((used))
+int8_t verify_key(uint8_t *key) {
+    bls::Bytes keyBytes = bls::Bytes(key, PRIVATE_KEY_LEN);
+    bls::PrivateKey sk = bls::PrivateKey::FromBytes(keyBytes);
+    bls::G1Element pk = sk.GetG1Element();
+
+    vector<uint8_t> message = {1, 2, 3, 4, 5};  // Message is passed in as a byte vector
+    bls::G2Element signature = bls::AugSchemeMPL().Sign(sk, message);
+
+    // Verify the signature
+    if(bls::AugSchemeMPL().Verify(pk, message, signature)) {
+        return 0;
+    } else {
+        return 1;
+    }
+}
+
+extern "C" __attribute__((visibility("default"))) __attribute__((used))
+uint8_t verify_signature(uint8_t *key, uint8_t *signature, uint8_t *message, size_t messageLen) {
+    bls::G1Element pk = bls::G1Element().FromBytes(bls::Bytes(key, PUBLIC_KEY_LEN));
+    bls::G2Element s = bls::G2Element().FromBytes(bls::Bytes(signature, SIGNATURE_LEN));
+    bool success = bls::AugSchemeMPL().Verify(pk, bls::Bytes(message, messageLen), s);
+    return success ? 0 : 1;
+}
---
+++ lib/core/bls/bls.dart
@@ -0,0 +1,139 @@
+import 'dart:ffi'; // For FFI
+import 'dart:io'; // For Platform.isX
+
+import 'package:convert/convert.dart';
+import 'package:ffi/ffi.dart';
+
+typedef NativeGetPublicKeyFuncType = Pointer<Uint8> Function(Pointer<Uint8>);
+typedef NativeSignFuncType = NativeByteArray Function(Pointer<Uint8>, NativeByteArray);
+typedef NativeVerifyFuncType = Int8 Function(Pointer<Uint8>);
+
+// library
+final nativeLib =
+    Platform.isAndroid ? DynamicLibrary.open("libbls.so") : DynamicLibrary.process();
+
+final nativeKeyGen = nativeLib
+    .lookupFunction<Pointer<Uint8> Function(), Pointer<Uint8> Function()>("key_gen");
+
+final nativeVerifySignature = nativeLib.lookupFunction<
+    Uint8 Function(Pointer<Uint8>, Pointer<Uint8>, Pointer<Uint8>, Uint64),
+    int Function(
+        Pointer<Uint8>, Pointer<Uint8>, Pointer<Uint8>, int)>("verify_signature");
+
+// sign with private key & message
+final nativeSign = nativeLib.lookupFunction<
+    Pointer<Uint8> Function(Pointer<Uint8>, Pointer<Uint8>, Uint64),
+    Pointer<Uint8> Function(Pointer<Uint8>, Pointer<Uint8>, int)>("sign");
+
+// native get public key from private key
+final NativeGetPublicKeyFuncType nativeGetPublicKey = nativeLib
+    .lookup<NativeFunction<NativeGetPublicKeyFuncType>>("get_public_key")
+    .asFunction();
+
+
+// verify verify
+final nativeVerify =
+    nativeLib.lookupFunction<Int8 Function(Pointer<Uint8>), int Function(Pointer<Uint8>)>(
+        "verify_key");
+
+final nativeTestString = nativeLib
+    .lookupFunction<Pointer<Utf8> Function(), Pointer<Utf8> Function()>("test_string");
+
+const PRIVATE_KEY_LEN = 32;
+const PUBLIC_KEY_LEN = 48;
+const SIGNATURE_LEN = 96;
+
+class NativeByteArray extends Struct {
+  Pointer<Uint8> data;
+
+  @Uint64()
+  int size;
+}
+
+class Bls {
+  List<int> _privateKey = [];
+  List<int> _publicKey = [];
+
+  Bls() {
+    final data = nativeKeyGen().asTypedList(81);
+    print(data);
+    final success = data[0];
+    if (success == 0) print("success");
+
+    this._privateKey = data.sublist(1, 33);
+    this._publicKey = data.sublist(33, 81);
+
+    print(this._privateKey);
+    print(this.privateKey);
+    print(this._publicKey);
+    print(this.publicKey);
+  }
+
+  Bls.fromPrivateKey(List<int> key) {
+    this._privateKey = key;
+    final Pointer<Uint8> pointer = calloc.allocate(_privateKey.length);
+    final pointerList = pointer.asTypedList(_privateKey.length);
+    pointerList.setAll(0, _privateKey);
+
+    this._publicKey = nativeGetPublicKey(pointer).asTypedList(PUBLIC_KEY_LEN);
+  }
+
+  List<int> get privateKey {
+    return _privateKey;
+  }
+
+  List<int> get publicKey {
+    return _publicKey;
+  }
+
+  String get privateKeyStr {
+    return hex.encode(_privateKey);
+  }
+
+  String get publicKeyStr {
+    return hex.encode(_publicKey);
+  }
+
+  Pointer<Uint8> listToPointer(List<int> list) {
+    final Pointer<Uint8> pointer = calloc.allocate(list.length);
+    final pointerList = pointer.asTypedList(list.length);
+    pointerList.setAll(0, list);
+    return pointer;
+  }
+
+  List<int> sign(List<int> message) {
+    print(message);
+    final Pointer<Uint8> msg = listToPointer(message);
+    final Pointer<Uint8> sk = listToPointer(this._privateKey);
+
+    final sign = nativeSign(sk, msg, message.length);
+
+    calloc.free(msg);
+    calloc.free(sk);
+
+    return sign.asTypedList(SIGNATURE_LEN);
+  }
+
+  bool verifySignature(List<int> signature, List<int> message) {
+    final Pointer<Uint8> msg = listToPointer(message);
+    final Pointer<Uint8> pk = listToPointer(this._publicKey);
+    final Pointer<Uint8> s = listToPointer(signature);
+
+    final ret = nativeVerifySignature(pk, s, msg, message.length);
+
+    calloc.free(msg);
+    calloc.free(pk);
+    calloc.free(s);
+
+    return ret == 0;
+  }
+
+  bool verify() {
+    final Pointer<Uint8> sk = listToPointer(this._privateKey);
+    final ret = nativeVerify(sk);
+
+    calloc.free(sk);
+
+    return ret == 0;
+  }
+}
